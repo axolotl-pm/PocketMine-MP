f777d2eb6b7d910e53d295e15a4301a4d2ab6a1b
IvanCraft623 <ivancraft623@gmail.com>
2026-08-14 19:16:19 -0600

    Update VersionInfo.php

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,8 +31,8 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "Axolotl-PM";
-	public const BASE_VERSION = "5.45.1";
-	public const IS_DEVELOPMENT_BUILD = true;
+	public const BASE_VERSION = "5.46.0";
+	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 	public const GITHUB_URL = "https://github.com/axolotl-pm/PocketMine-MP";
 
